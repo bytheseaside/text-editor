8c0287e44aa992e10a4eb93d5d401e876154f824
Brisa Rojas <rojaasbrisa@gmail.com>
2025-01-08 22:30:01 +0100

    refactor state

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,14 @@
 'use client'
 
 import { marked } from 'marked';
-import { useState } from "react";
 import { EditorProvider } from '@tiptap/react'
 import Box from '@mui/material/Box'
 import StarterKit from "@tiptap/starter-kit";
 import MenuBar from "./components/MenuBar";
 import CustomFloatingMenu from './components/CustomFloatingMenu';
 
-// const URL = 'https://backend.app.qanswer.ai/api/tasks/report-copilot/report-continuation'
-
 export default function Home() {
-  const [info, setInfo] = useState('# Editor Styles \nHere you can test all the styles applied from the menu bar: \n## Heading 2 \nThis is an *italicized* text, and here is a **bold** text. \nHere is a `console.log()` statement: \n```\nconst foo = \'bar\';\n```\n---\n- List item 1\n- List item 2\n\n1. Ordered list item 1\n2. Ordered list item 2\n\n*Emphasized* and **strong** text in paragraphs.')
+  const startText = '# Editor Styles \nHere you can test all the styles applied from the menu bar: \n## Heading 2 \nThis is an *italicized* text, and here is a **bold** text. \nHere is a `console.log()` statement: \n```\nconst foo = \'bar\';\n```\n---\n- List item 1\n- List item 2\n\n1. Ordered list item 1\n2. Ordered list item 2\n\n*Emphasized* and **strong** text in paragraphs.'
 
   return (
     <Box
@@ -46,7 +43,7 @@ export default function Home() {
           <MenuBar />
         }
         extensions={[StarterKit]}
-        content={marked(info)}
+        content={marked(startText)}
         editorContainerProps={{
           style: {
             padding: '24px', // General padding for the editor
